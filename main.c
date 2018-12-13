848e5e63f79fc6e1602b2e6edea0bec241f7bcd0
lukecombe <45853498+lukecombe@users.noreply.github.com>
2018-12-13 23:49:36 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -7,6 +7,122 @@ typedef int bool;
 #define TRUE  1
 #define FALSE 0
 
+struct Contact
+{
+    char name[30];
+    char firstname[30];
+    char phonenumber[20];
+    char address[100];
+};
+
+//fonction parcourir
+//permet de parcourir l'annuaire
+void browse(){
+    // declaration de variable
+    struct Contact users[20];
+    int LineCount = 0;
+    int car = 0;
+    int curPos = 0;
+    int contactNumber = 0;
+    int choice=0;
+    int c;
+    bool stay = TRUE;
+    bool suivant = FALSE;
+    bool precedent = FALSE;
+
+    FILE* file = fopen("C:\\Users\\Luke\\Documents\\Projets\\Annuaire\\Annuaire.txt","r");
+    if (file != NULL){
+        do
+        {
+            car = fgetc(file);
+            if (car == '\n')
+                LineCount++;
+        } while(car != EOF);
+        //printf("Nombre de lignes %d : ", LineCount);
+
+        if (LineCount == 0){
+            printf("Annuaire vide \n");
+            //on sort de la fonction sans oublier de fermer le fichier
+            fclose(file);
+            return;
+        }
+
+        // on se repositionne en début de fichier
+        fseek(file, 0, SEEK_SET);
+
+        //remplissage du tableau
+        for (curPos = 0; curPos < LineCount; curPos++)
+        {
+            fscanf(file, "%s %s %s %s", users[curPos].name, users[curPos].firstname, users[curPos].phonenumber, users[curPos].address);
+        }
+
+        while(stay) {
+            //affichage du contact courant
+            printf("contact %i sur %i : %s\t%s\t%s\t%s\n", contactNumber + 1, LineCount, users[contactNumber].name,
+                   users[contactNumber].firstname, users[contactNumber].phonenumber, users[contactNumber].address);
+
+            suivant = contactNumber < LineCount -1;
+            //printf("\ncontactNumber : -%i-\n", contactNumber);
+            //printf("\nLineCount : -%i-\n", LineCount);
+            precedent = contactNumber > 0;
+            //printf("suivant = %i\n", suivant);
+            //printf("precedent = %i\n", precedent);
+
+            //s'il existe un contact suivant
+            if (suivant) {
+                //s'il existe un contact précédent
+                if (precedent) {
+                    printf("Entrez 'p' pour precedent, 's' pour suivant, 'q' pour quitter la navigation\n");
+                    //s'il n'existe pas de contact précédent
+                } else {
+                    printf("Aucun precedent. Entrez 's' pour suivant, 'q' pour quitter la navigation\n");
+                }
+                //s'il n'existe pas de contact suivant
+            } else {
+                //s'il existe un contact précédent
+                if (precedent) {
+                    printf("Pas de suivant. Entrez 'p' pour precedent, 'q' pour quitter la navigation\n");
+                    //s'il n'existe pas de contact précédent
+                } else {
+                    printf("Pas de suivant, pas de precedent. Entrez 'q' pour quitter la navigation\n");
+                }
+            }
+
+            printf("> ");
+            while ((c = getchar()) != '\n' && c!= EOF);
+            c = getchar();
+            //printf("\nwhile - le choix est : -%i-\n", c);
+
+            //on teste la valeur du caractère ASCII
+            if (c == 112) choice = 1;
+            if (c == 115) choice = 2;
+            if (c == 113) choice = 3;
+
+            switch (choice) {
+                case 1:
+                    //printf("precedent\n");
+                    if (precedent) contactNumber--;
+                    break;
+                case 2:
+                    //printf("suivant \n");
+                    if (suivant) contactNumber++;
+                    break;
+                case 3:
+                    //printf("quitter\n");
+                    stay = FALSE;
+                    break;
+                default:
+                    printf("Mauvaise entree\n");
+                    break;
+                }
+            fclose(file);
+        }
+    } else{
+        printf("browse:Erreur ouverture ficher\n");
+    }
+}
+
+
 void add_contact(){
     // declaration de variables
     char name[30];
@@ -32,69 +148,92 @@ void add_contact(){
         scanf("%s", &address);
 
     //write new entry in file
-    FILE* file = fopen("C:\\Users\\Luke\\Documents\\Projets\\Annuaire\\Annuaire.csv","a");
-    fprintf(file, "\n%s;%s;%s;%s",name,firstname,phonenumber,address);
-  //  char c;
-   // do{
-    //    c = fgetc(file);
-    //    printf("%c", c);
-   // }
-  //  while (c != EOF);
+    FILE* file = fopen("C:\\Users\\Luke\\Documents\\Projets\\Annuaire\\Annuaire.txt","a");
+    fprintf(file, "%s %s %s %s\n",name,firstname,phonenumber,address);
 
     fclose(file);
-   // return 0 ;
-
 }
 
-//void parcourir() {
 
-  //  FILE* file = fopen("C:\\Users\\Luke\\Documents\\Projets\\Annuaire\\Annuaire.csv", "r+");
-  void search_contact()
-  {
-      // declaration de variables
-      char name[30];
-      int TAILLE_MAX=25 ;
-      char chaine[TAILLE_MAX];
-      int offset = 0;
+void search_contact()
+{
+  // declaration de variables
+    struct Contact users[20];
+    int LineCount = 0;
+    int car = 0;
+    int curPos = 0;
+    int contactNumber = 0;
+    int c = 0;
+    char name = "";
+    bool keepOnSearching = TRUE;
 
-      //ouverture du fichier en lecture seule
-      FILE* file = fopen("C:\\Users\\Luke\\Documents\\Projets\\Annuaire\\Annuaire.csv","r");
-      // on ne recherche que par le nom
-      //TO DO ajouter d'autres critères si nécessaire
-      //on demande le nom
-      printf("Veuillez rentrer le nom du nouveau contact :\n");
-      while( getchar() != '\n');
-        scanf("%s", &name);
+//    printf("search_contact\n");
 
-        rewind(file);
-        if(fgets(chaine,TAILLE_MAX,file)!=NULL)
+    //ouverture du fichier en lecture/écriture
+  FILE* file = fopen("C:\\Users\\Luke\\Documents\\Projets\\Annuaire\\Annuaire.txt","r");
+    if (file != NULL){
+        do
         {
-          strlwr(chaine);
-          strlwr(name);
-          if(strstr(chaine,name)!=NULL)
-          {
-              printf("Donnee non trouve\n");
-          }
-          else
-          {
-              offset = fseek(file,1,SEEK_SET);
-              printf("Le decalage est de %i\n", offset);
-          }
+            car = fgetc(file);
+            if (car == '\n')
+                LineCount++;
+        } while(car != EOF);
+        printf("Nombre de lignes %d : ", LineCount);
+
+        if (LineCount == 0){
+            printf("Annuaire vide \n");
+            //on sort de la fonction sans oublier de fermer le fichier
+            fclose(file);
+            return;
         }
-      fclose(file);
-      }
+
+        // on se repositionne en début de fichier
+        fseek(file, 0, SEEK_SET);
+
+        //remplissage du tableau
+        for (curPos = 0; curPos < LineCount; curPos++)
+        {
+            fscanf(file, "%s %s %s %s", users[curPos].name, users[curPos].firstname, users[curPos].phonenumber, users[curPos].address);
+            //printf("contact %i sur %i : %s\t%s\t%s\t%s\n", curPos + 1, LineCount, users[curPos].name,
+                   //users[curPos].firstname, users[curPos].phonenumber, users[curPos].address);
+        }
+
+        // on ne recherche que par le nom
+        //TO DO ajouter d'autres critères si nécessaire
+        //on demande le nom
+        printf("Veuillez rentrer le nom du contact a rechercher :\n");
+        while( getchar() != '\n');
+            scanf("%s", &name);
+
+        //printf("recherche - name = -%s-\n", &name);
+
+        //on parcourt le tableau pour rechercher une entree correspondante
+        while ((keepOnSearching) && (contactNumber < LineCount)) {
+            //printf("recherche - while - name -%s-\n", &name);
+            //printf("recherche - while - users[contactNumber].name -%s-\n", users[contactNumber].name);
+            if (strcmp(users[contactNumber].name, &name) == 0){
+                //printf("recherche - users[contactNumber].name = -%s-", users[contactNumber].name);
+                //printf("TROUVE\n");
+                keepOnSearching = FALSE;
+                printf("contact TROUVE %i sur %i : %s\t%s\t%s\t%s\n\n", contactNumber + 1, LineCount, users[contactNumber].name,
+                       users[contactNumber].firstname, users[contactNumber].phonenumber, users[contactNumber].address);
+            } else {
+                contactNumber++;
+            }
+        }
+    } else {
+        printf("browse:Erreur ouverture ficher\n");
+    }
+  fclose(file);
+  }
 
 
-
-
-
-//}
-
-void supprimer() {
-
-    FILE* file = fopen("C:\\Users\\Luke\\Documents\\Projets\\Annuaire\\Annuaire.csv", "w+");
+void deleteAllContacts() {
+    //printf("deleteAllContacts\n");
+    FILE* file = fopen("C:\\Users\\Luke\\Documents\\Projets\\Annuaire\\Annuaire.txt", "w+");
     fclose(file);
 }
+
 
 void main() {
     int choice;
@@ -124,22 +263,23 @@ void main() {
 
        switch (choice) {
            case 1:
-               printf("parcourir\n");
+               //printf("parcourir\n");
+               browse();
                break;
            case 2:
-               printf("ajouter \n");
+               //printf("ajouter \n");
                add_contact();
                break;
            case 3:
-               printf("rechercher\n");
+               //printf("rechercher\n");
                search_contact();
                break;
            case 4:
-               printf("supprimer\n");
-               supprimer();
+               //printf("supprimer\n");
+               deleteAllContacts();
                break;
            case 5:
-               printf("quitter\n");
+               //printf("quitter\n");
                stay = FALSE;
                break;
            default:
